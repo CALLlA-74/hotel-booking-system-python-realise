e55bab1f9f050cfc7dc0ff1c61f25b2e6735ca67
CALLlA-74 <56507954+CALLlA-74@users.noreply.github.com>
2025-03-25 18:20:36 +0300

    Update config.py

--- services/statistic_service/app/config/config.py
+++ services/statistic_service/app/config/config.py
@@ -15,3 +15,9 @@ def get_db_url(config_file_name: str = "./app/config/config.yaml"):
                         f"{settings['host']}:" \
                         f"{settings['port']}/" \
                         f"{settings['db_name']}"
+
+
+def get_kafka_settings(config_file_name: str = "./app/config/config.yaml", service_config_name: str = 'kafka_config'):
+    with open(config_file_name) as file:
+        data = safe_load(file)
+    return data[service_config_name]
